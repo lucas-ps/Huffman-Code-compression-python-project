145d73a40faa976ac746f8748b8851a624ce6017
Lucas Proudhon Smith <75811985+lucas-ps@users.noreply.github.com>
2021-03-24 00:58:10 +0000

    Re-did huffman tree generation method,  added read text from file method, added documentation

--- Node.py
+++ Node.py
@@ -1,13 +1,41 @@
 # Tree node class for storage of characters
 class Node:
-    def __init__(self, left=None, right=None):
+    def __init__(self, char, left=None, right=None, frequency=None):
         """
          Constructor for a new node, creates a node in the tree with defined child nodes
         :param left:
         :param right:
         """
+        self.char = char
         self.left = left
         self.right = right
+        if frequency is None:
+            frequency = self.calcFrequency()
+        self.frequency = frequency
+
+    # Override the `__lt__()` function to make `Node` class work with priority queue
+    # such that the highest priority item has the lowest frequency
+    # TODO: Check this
+    def __lt__(self, other):
+        return self.frequency < other.frequency
+
+    def calcFrequency(self) -> int:
+        """
+        Calculates the frequency of a non leaf node (when frequency is not provided)
+        """
+        try:
+            if self.left is not None:
+                if self.right is not None:
+                    frequency = self.left[0].getFrequency() + self.right[0].getFrequency()
+                else:
+                    frequency = self.left[0].getFrequency()
+            elif self.right is not None:
+                frequency = self.right[0].getFrequency()
+            else:
+                raise Exception("Node provided is a leaf node, no frequency was provided")
+        except:
+            frequency = 0
+        return frequency
 
     def children(self):
         """
@@ -15,12 +43,6 @@ class Node:
         :return: Node objects of the node's children
         """
         return self.left, self.right
-
-    def getCharacter(self):
-        """
-        :return: The node object's character
-        """
-        return self.character
 
     def nodes(self):
         """
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from typing import List, Dict
-
+from typing import List, Dict, Any
 from Node import Node
 import queue
+import heapq
+
+
+def get_text_from_file(file: str) -> str:
+    """
+    :return: String of text data in provided file
+    """
+    file_data = open(file, "r").readlines()
+    file_data_str = ''.join(map(str, file_data))
+    return file_data_str
 
 
 # Calculate frequencies of each letter in string provided
@@ -18,28 +27,42 @@ def calc_letter_frequencies(text: str) -> list:
         else:
             char_frequencies[character] = 1
 
-    # Sorting the dictionary in descending order of frequencies
+    # Sorting the dictionary in ascending order of frequencies
     char_frequencies_sorted: List[tuple] = sorted(char_frequencies.items(), key=lambda x: x[1])
     return char_frequencies_sorted
 
 
-# Creating hoffman tree 
-def create_tree(text: str) -> Node:
+# Creating hoffman tree
+def create_huffman_tree(text: str) -> Node:
+    """
+    Creates all necessary nodes to construct a huffman coding tree, returns the root node of the tree
+    :param text: The provided string text
+    :return: The root node of the tree
+    """
     char_frequencies = calc_letter_frequencies(text)
-    while len(char_frequencies) > 1:  # Until only root node remains
-        left_node = char_frequencies[0]
-        right_node = char_frequencies[1]  # Getting two nodes with lowest frequency
-        node = Node(left_node, right_node)  # Creating the node
-        frequencies_to_remove = [char_frequencies[0], char_frequencies[1]]
-        char_frequencies = char_frequencies - frequencies_to_remove  # Removing processed characters
-        node_frequency = (node, left_node[1] + right_node[1])
-        char_frequencies.append(node_frequency)  # Adding new node object back into frequency list
-        char_frequencies = sorted(char_frequencies.items(), key=lambda x: x[1])  # Sorting list by ascending frequency
-    root_node = char_frequencies[0][0]
-    return root_node
+    # Creating queue of letters and their frequencies
+    queue = []
+    for character, frequency in char_frequencies.items():
+        queue.append(Node(character, frequency))
+    heapq.heapify(queue)
+
+    while len(queue) > 1:  # Until only root node remains
+        # Choosing 2 letters with lowest frequencies for child nodes, removing them from queue
+        left_node = heappop(queue)
+        right_node = heappop(queue)
+        combined_frequency_of_children = left_node.frequency + right_node.frequency
+        heappush(queue, Node(None, left_node, right_node, combined_frequency_of_children))
+
+    return queue[0]  # Returning root node of tree
 
 
-# Testing code above with sample text
+# Main code to run all of the above
 if __name__ == '__main__':
-    text = "text123 this is a test"
-    print(calc_letter_frequencies(text))
+    text = ""
+    while text == "":
+        file = input("Which file would you like to encode eg. file.txt (must be in current program directory)")
+        try:
+            text = get_text_from_file(file)
+        except:
+            print("File '" + file + "' was not found, or the file was empty. Please enter a valid filename")
+    root_node = create_huffman_tree(text)
